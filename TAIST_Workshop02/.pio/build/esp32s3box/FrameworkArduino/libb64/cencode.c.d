ac049a84e1fc450b57af5ab948bd03b47b1c96e7
PeetKiattisak <peetlove77@hotmail.com>
2024-03-18 21:54:56 +0700

    delete some file

--- TAIST_Workshop02/.pio/build/esp32s3box/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32s3box/FrameworkArduino/libb64/cencode.c.o: \
- D:/Users/watthanail/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- D:/Users/watthanail/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
